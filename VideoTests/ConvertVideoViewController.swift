2b69dc1596f6fb4c83b3522637fd11a4fc222c24
Pedro Sánchez <pedro.sanchez.it@gmail.com>
2019-05-14 12:43:17 +0100

    [ADD] Crop video to square

--- VideoTests/ConvertVideoViewController.swift
+++ VideoTests/ConvertVideoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ConvertVideoViewController: UIViewController {
     
@@ -31,6 +32,61 @@ class ConvertVideoViewController: UIViewController {
     
     
     @IBAction func converToSquare(_ sender: Any) {
+        cropVideoToSquare()
+    }
+    
+    func cropVideoToSquare () {
+        //output file
+        let docFolder : String? = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last
+       // let outputPath = URL(fileURLWithPath: docFolder ?? "").appendingPathComponent("output2.mov").absoluteString
+        var outputPath = ""
+        if let docFolder = docFolder {
+        outputPath = docFolder
+        outputPath.append("/output2.mov")
+        }
+        print(outputPath)
+        if FileManager.default.fileExists(atPath: outputPath) {
+            do {
+                try FileManager.default.removeItem(atPath: outputPath)
+            } catch {
+                print("Error output file")
+            }
+        }
+        
+        //input file
+        let asset = AVAsset(url: videoURL!)
+        
+        let composition = AVMutableComposition()
+        composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        //input clip
+        let clipVideoTrack: AVAssetTrack = asset.tracks(withMediaType: .video)[0]
+        
+        //make it square
+        let videoComposition = AVMutableVideoComposition(propertiesOf: asset)
+        videoComposition.renderSize = CGSize(width: clipVideoTrack.naturalSize.height, height: clipVideoTrack.naturalSize.height)
+        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: 30)
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRangeMake(start: CMTime.zero, duration: CMTimeMakeWithSeconds(60, preferredTimescale: 30))
+        
+        //rotate to portrait
+        
+        //export
+        let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality)
+        exporter?.videoComposition = videoComposition
+        exporter?.outputURL = URL(fileURLWithPath: outputPath)
+        exporter?.outputFileType = .mov
+        
+        exporter?.exportAsynchronously(completionHandler: {
+            print("Exporting done!")
+            if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(outputPath) {
+                UISaveVideoAtPathToSavedPhotosAlbum(outputPath, nil, nil, nil)
+            }
+            
+        })
+
+        
     }
     
     
